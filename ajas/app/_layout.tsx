a051e3b84d2f34fda3c302b0cfe15e44988ebc8b
bigfood <satcom1600@hanmail.net>
2026-01-30 16:29:06 +0900

    Revert "WIP: install notifications"
    This reverts commit 7ef132cdaf8a43d9db78a31842179040845a17e2.

--- ajas/app/_layout.tsx
+++ ajas/app/_layout.tsx
@@ -1,51 +1,22 @@
-import {
-  DarkTheme,
-  DefaultTheme,
-  ThemeProvider,
-} from "@react-navigation/native";
-import { Stack } from "expo-router";
-import { StatusBar } from "expo-status-bar";
-import "react-native-reanimated";
+import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
+import { Stack } from 'expo-router';
+import { StatusBar } from 'expo-status-bar';
+import 'react-native-reanimated';
 
-import { useColorScheme } from "@/hooks/use-color-scheme";
-import { useEffect } from "react";
-import { Notifications } from "react-native-notifications";
+import { useColorScheme } from '@/hooks/use-color-scheme';
 
 export const unstable_settings = {
-  anchor: "(tabs)",
+  anchor: '(tabs)',
 };
 
 export default function RootLayout() {
   const colorScheme = useColorScheme();
 
-  useEffect(() => {
-    Notifications.registerRemoteNotifications();
-
-    Notifications.events().registerNotificationReceivedForeground(
-      (notification, completion) => {
-        console.log(
-          `Notification received in foreground: ${notification.title} : ${notification.body}`,
-        );
-        completion({ alert: false, sound: false, badge: false });
-      },
-    );
-
-    Notifications.events().registerNotificationOpened(
-      (notification, completion) => {
-        console.log(`Notification opened: ${notification.payload}`);
-        completion();
-      },
-    );
-  }, []);
-
   return (
-    <ThemeProvider value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
+    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
       <Stack>
         <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen
-          name="modal"
-          options={{ presentation: "modal", title: "Modal" }}
-        />
+        <Stack.Screen name="modal" options={{ presentation: 'modal', title: 'Modal' }} />
       </Stack>
       <StatusBar style="auto" />
     </ThemeProvider>
