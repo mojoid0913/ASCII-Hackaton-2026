47d4b52381cbe02428c1b7694f881e48e2c62084
bigfood <satcom1600@hanmail.net>
2026-01-30 16:18:35 +0900

    WIP: setup fcm

--- ajas/app/_layout.tsx
+++ ajas/app/_layout.tsx
@@ -8,8 +8,7 @@ import { StatusBar } from "expo-status-bar";
 import "react-native-reanimated";
 
 import { useColorScheme } from "@/hooks/use-color-scheme";
-import { useEffect } from "react";
-import { Notifications } from "react-native-notifications";
+import NotificationListener from "@/components/NotificationListener";
 
 export const unstable_settings = {
   anchor: "(tabs)",
@@ -18,28 +17,9 @@ export const unstable_settings = {
 export default function RootLayout() {
   const colorScheme = useColorScheme();
 
-  useEffect(() => {
-    Notifications.registerRemoteNotifications();
-
-    Notifications.events().registerNotificationReceivedForeground(
-      (notification, completion) => {
-        console.log(
-          `Notification received in foreground: ${notification.title} : ${notification.body}`,
-        );
-        completion({ alert: false, sound: false, badge: false });
-      },
-    );
-
-    Notifications.events().registerNotificationOpened(
-      (notification, completion) => {
-        console.log(`Notification opened: ${notification.payload}`);
-        completion();
-      },
-    );
-  }, []);
-
   return (
     <ThemeProvider value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
+      <NotificationListener />
       <Stack>
         <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
         <Stack.Screen
