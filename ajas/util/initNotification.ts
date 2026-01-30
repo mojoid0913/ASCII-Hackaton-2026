2b642e7826f3e59aed25cd5028cdf5360dc8d71d
bigfood <satcom1600@hanmail.net>
2026-01-30 23:32:20 +0900

    Remove debries

--- ajas/util/initNotification.ts
+++
@@ -1,7 +0,0 @@
-import NotificationsModule from "@/modules/notifications/src/NotificationsModule";
-
-export default async function initNotification() {
-  if (NotificationsModule.isPermissionGranted())
-    NotificationsModule.isListenerConnected();
-  NotificationsModule.requestPermission();
-}
